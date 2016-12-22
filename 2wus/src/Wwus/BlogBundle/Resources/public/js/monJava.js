7d0cecd9e73c0e378d09d298509c44ce4fa991f8
garrytopper <yannickpro64@gmail.com>
2016-12-22 11:43:48 +0100

    création d'un alerte pour connaitre la taille de l'écran lorsque on clic sur la section1

--- 2wus/src/Wwus/BlogBundle/Resources/public/js/monJava.js
+++ 2wus/src/Wwus/BlogBundle/Resources/public/js/monJava.js
@@ -1,5 +1,8 @@
 $(function() {
-    $("#section1").on("clic",function(){
-      alert('test');
+    $("#section1").on("click",function(){
+        var X_ecran = document.documentElement.clientWidth;
+        var Y_ecran = document.documentElement.clientHeight;
+        var element = document.activeElement;
+        alert('La largeur de l\'écran est de ' + X_ecran + ' et sa hauteur est de ' + Y_ecran + ' et l\'élément courant est : ' + element);
     }); 
 });
