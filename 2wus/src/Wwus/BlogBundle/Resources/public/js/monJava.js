26f90fe4078e3065205c50aa08054f87f598b0f8
garrytopper <yannickpro64@gmail.com>
2016-12-22 19:05:28 +0100

    modifications des règles de css et js pour redimenssionner les div de la page d'accueil

--- 2wus/src/Wwus/BlogBundle/Resources/public/js/monJava.js
+++ 2wus/src/Wwus/BlogBundle/Resources/public/js/monJava.js
@@ -1,6 +1,23 @@
 $(function() {
-    // Code exécuté avant même que la page soit chargée
+    // Code exécuté avant même que la page soit chargée je redimensionne les div
+    // de la page d'accueil
+    // En premier le redimenssionnement de mes sections en fonction de la taille de l'écran
     var X = $(window).width();
     var Y = $(window).height();
     $('#section1, #section2, #section3').css('height', Y).css('width', '100%');
+    // Ensuite je redimensionne le contenu de la section1: "contenu" et "scroll" (scroll est définit en css)
+    var Yscroll = $('.scroll').height() ;
+    var Ycontenu = Y - Yscroll ;
+    $('.contenu').css('height', Ycontenu);
+    // Ensuite je redimenssionne le contenu de "contenu" (logo est définit en css)
+    var Ydescription = $('#description_accueil').height();
+    var Ylogo = $('#logo_accueil').height();
+    // #tampon_accueil s'adaptera en fonction du contenu de #description_accueil définit en css
+    // le - 4 c'est pour palier une différence que je présume du à la séparation en 2 row
+    var Ytampon = Ycontenu - Ydescription - Ylogo - 4;
+    $('#tampon_accueil').css('height', Ytampon);
+
+    //***************************************** TEST
+    //alert('contenu : '+ Ycontenu +' dont logo : '+ Ylogo +', tampon : '+ Ytampon +', description : '+ Ydescription);
+    //alert('contenu : '+ Ycontenu +', + scroll : '+ Yscroll +' devrait faire : ' + Y);
 });
