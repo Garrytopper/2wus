afbe563cb2400f94aa734fb341aaf3f2f56d6715
garrytopper <yannickpro64@gmail.com>
2016-12-25 20:46:15 +0100

    modification de la page d'accueil en modifiant les règles css et js

--- 2wus/src/Wwus/BlogBundle/Resources/public/js/monJava.js
+++ 2wus/src/Wwus/BlogBundle/Resources/public/js/monJava.js
@@ -5,19 +5,28 @@ $(function() {
     var X = $(window).width();
     var Y = $(window).height();
     $('#section1, #section2, #section3').css('height', Y).css('width', '100%');
-    // Ensuite je redimensionne le contenu de la section1: "contenu" et "scroll" (scroll est définit en css)
-    var Yscroll = $('.scroll').height() ;
-    var Ycontenu = Y - Yscroll ;
+    // Ensuite je redimensionne le contenu de la section1: "contenu"  
+    var YpiedPage = $('.pied-de-page').height();
+    var YenTete = $('.en-tete').height();
+    var Ycontenu = Y - YpiedPage - YenTete ;
     $('.contenu').css('height', Ycontenu);
-    // Ensuite je redimenssionne le contenu de "contenu" (logo est définit en css)
-    var Ydescription = $('#description_accueil').height();
-    var Ylogo = $('#logo_accueil').height();
-    // #tampon_accueil s'adaptera en fonction du contenu de #description_accueil définit en css
-    // le - 4 c'est pour palier une différence que je présume du à la séparation en 2 row
-    var Ytampon = Ycontenu - Ydescription - Ylogo - 4;
-    $('#tampon_accueil').css('height', Ytampon);
+    
+    // Ensuite je redimenssionne le contenu de la section1 
+        var Ydescription = $('#description_accueil').height();
+        var Ylogo = $('#logo_accueil').height();
+        // #tampon_accueil s'adaptera en fonction du contenu de #description_accueil définit en css
+        // le - 4 c'est pour palier une différence que je présume du à la séparation en 2 row
+        var Ytampon = Ycontenu - Ydescription - Ylogo - 4;
+        $('#tampon_accueil').css('height', Ytampon);
+    
+    // Ensuite je redimenssionne le contenu de la section2
+    var Ycarroussel = Ycontenu * 0.7 ;
+    var Yliste = Ycontenu * 0.3 ;
+    $('#carroussel').css('height', Ycarroussel);
+    $('#liste_article').css('height', Yliste);
 
+    
     //***************************************** TEST
-    //alert('contenu : '+ Ycontenu +' dont logo : '+ Ylogo +', tampon : '+ Ytampon +', description : '+ Ydescription);
+    //alert('contenu : '+ Ycontenu +' dont en-tête : '+ YenTete +', et le pied-de-page : '+ YpiedPage);
     //alert('contenu : '+ Ycontenu +', + scroll : '+ Yscroll +' devrait faire : ' + Y);
 });
